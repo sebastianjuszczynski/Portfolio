f3efc09e93ee6df355ee7eb6ca9c042a98e02672
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2025-05-30 13:39:57 +0200

    Add skills section with layout, styles, and translations

--- js/i18n.js
+++ js/i18n.js
@@ -10,6 +10,11 @@ export const translations = {
     aboutTitle: "About me",
     aboutDescription: "I’m a self-taught frontend developer focused on writing clean, accessible code and building responsive websites. I love turning ideas into real interfaces and constantly learning new tools and technologies.",
     aboutButton: "Contact me",
+    skillsSubtitle: "My",
+    skillsSubtitleSpan: "tech stack",
+    skillsTitle: "Skills",
+    skillsInfo: "Visit the projects section to see the work done with there web technologies."
+
 
     
   },
@@ -23,8 +28,11 @@ export const translations = {
     aboutSubtitleSpan: "słów",
     aboutTitle: "O mnie",
     aboutDescription: "Jestem początkującym frontend developerem, który uczy się samodzielnie i skupia się na pisaniu przejrzystego, dostępnego kodu oraz tworzeniu responsywnych stron. Lubię zamieniać pomysły w realne interfejsy i nieustannie uczę się nowych narzędzi i technologii.",
-    aboutButton: "Napisz do mnie"
-
+    aboutButton: "Napisz do mnie",
+    skillsSubtitle: "Mój",
+    skillsSubtitleSpan: "tech stack",
+    skillsTitle: "Umiejętności",
+    skillsInfo: "Sprawdź sekcję z projektami, aby zobaczyć efekty pracy z tymi technologiami."
 
   }
 };
